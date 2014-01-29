51e05203ad161c5caca02d25b178041b57973cd5
Natas <oxnard@me.com>
2014-01-29 14:29:58 -0800

    Begin Gruntfile

---
+++ app/templates/Gruntfile.js
@@ -0,0 +1,22 @@
+/*!
+ * Generator-Gnar Gruntfile
+ * http://gnarmedia.com
+ * @author Adam Murphy
+ */
+
+// Generated on <%= (new Date).toISOString().split('T')[0] %> using <%= pkg.name %> <%= pkg.version %>
+
+'use strict';
+
+/**
+ * Grunt module
+ */
+module.exports = function (grunt) {
+
+  /**
+   * Generator-Gnar Grunt config
+   */
+  grunt.initConfig({
+  ]);
+
+};
