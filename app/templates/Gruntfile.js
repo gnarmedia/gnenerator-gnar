f8f33e202b70210a5ef18d1f5d5758a5e87b69ec
Adam Murphy <oxnard@me.com>
2014-01-31 19:16:21 -0800

    Remove unneeded project properties

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -29,13 +29,6 @@ module.exports = function (grunt) {
     project: {
       src: 'src',
       dest: 'build',
-      assets: '<%= project.dest %>/assets',
-      css: [
-        '<%= project.src %>/scss/style.scss'
-      ],
-      js: [
-        '<%= project.src %>/js/*.js'
-      ],
       html: [
         '<%= project.src %>/html/*.html'
       ]
